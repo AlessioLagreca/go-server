328bed4e00c85b6e8106dd65d76f806e4423690e
Alessio Lagreca <82826676+AlessioLagreca@users.noreply.github.com>
2024-05-28 20:34:07 +0200

    first commit

---
+++ server.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {	
+	fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	
+	http.ListenAndServe(":80", nil)
+}
+
