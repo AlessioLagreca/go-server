c1d8c1a2e7601819bd8f860d8a8432ef2eed421a
Alessio Lagreca <82826676+AlessioLagreca@users.noreply.github.com>
2024-06-06 15:57:46 +0200

    added html file to response

--- server.go
+++ server.go
@@ -2,16 +2,48 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {	
-	fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
+
+	log.Printf("Received request: %s %s\n", r.Method, r.URL.Path)
+
+	// definire il percorso del file
+	path := r.URL.Path
+
+	// se il percorso è index.html o / allora restituisci index.html
+	if path == "/" || path == "index.html" {
+		path = "index.html"
+	}
+
+	// restituisci il file
+	http.ServeFile(w, r, path)
+
+
+	// log all headers
+	for key, values := range r.Header {
+		for _, value := range values {
+			log.Printf("Header: %s: %s\n", key, value)
+		}
+	}
+	// crea la risposta
+	response := fmt.Sprintf("Requested path: %s\r\n", r.URL.Path)
+	w.Write([]byte(response))
+	
+	 // Crea lo status code
+	 w.WriteHeader(http.StatusOK)
 }
+
+// FUNZIONE MAIN
 
 func main() {
 	http.HandleFunc("/", handler)
 	
-	http.ListenAndServe(":80", nil)
+	log.Println("Starting server on :8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatalf("could not start server: %s\n", err)
+	}
 }
 
